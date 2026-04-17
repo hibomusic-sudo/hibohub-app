8d50fba5096d2debf0625952df69f5ba38450d84
Hibo Music <hibomusic0@gmail.com>
2026-04-17 05:32:53 +0000

    Set up a Firebase backend

---
+++ src/firebase/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  "projectId": "studio-9738489573-8bd2e",
+  "appId": "1:227660214569:web:00ce7b7d518793544f4738",
+  "apiKey": "AIzaSyBn4Mo7QkljXa1qUIAXVMsT1FGoTo--OYc",
+  "authDomain": "studio-9738489573-8bd2e.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "227660214569"
+};
